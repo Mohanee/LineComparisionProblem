e2d5f54c1c1c50610d569b625094533c18889a28
Mohanee <mohanee99@gmail.com>
2020-09-30 09:49:04 +0530

    [mohanee] add welcome file

---
+++ LineComp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program on Master Branch!!!!");
+        }
+    }
+}
