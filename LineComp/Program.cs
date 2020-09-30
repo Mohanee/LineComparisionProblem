dbac01380ae3fa83acad97fa3b8cd4c3111de878
Mohanee <mohanee99@gmail.com>
2020-09-30 10:05:02 +0530

    [mohanee] add compare using equal method

--- LineComp/Program.cs
+++ LineComp/Program.cs
@@ -6,14 +6,24 @@ namespace LineComp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("**USE CASE 1**")
+            Console.WriteLine("**USE CASE 2**");
             Console.WriteLine("Welcome to Line Comparision Computation Program"+"\n"+"We'll be calculating length between 2 points");
             int x1 = 3, x2 = 6;
             int y1 = 4, y2 = 9;
-            double length = 0;
+            int x3 = 5, x4 = 10;
+            int y3 = 8, y4 = 12;
+            double length1 = 0, lenght2 = 0;
 
-            length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("Length of the line is " + length);
+            length1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            lenght2 = Math.Sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+            if (lenght2.Equals(length1))
+            {
+                Console.WriteLine("Both lines are of equal length");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are of different length");
+            }
         }
     }
 }
