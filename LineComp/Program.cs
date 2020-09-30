86423a9ed6f0d1d97b9045208198256bf9b51897
Mohanee <mohanee99@gmail.com>
2020-09-30 09:58:12 +0530

    [mohanee] add program to calculate length btw 2 pts

--- LineComp/Program.cs
+++ LineComp/Program.cs
@@ -6,7 +6,13 @@ namespace LineComp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Computation Program on Master Branch!!!!");
+            Console.WriteLine("Welcome to Line Comparision Computation Program"+"\n"+"We'll be calculating length between 2 points");
+            int x1 = 3, x2 = 6;
+            int y1 = 4, y2 = 9;
+            double length = 0;
+
+            length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("Length of the line is " + length);
         }
     }
 }
