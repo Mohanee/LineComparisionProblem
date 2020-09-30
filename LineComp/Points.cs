063baebc460f367b52b1f605e45507dd866f6df9
Mohanee <mohanee99@gmail.com>
2020-09-30 14:47:52 +0530

    [mohanee] refactor code

---
+++ LineComp/Points.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComp
+{
+    class Points
+    {
+        int x1, y1, x2, y2;
+        double length;
+
+        public Points(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public double calLength()
+        {
+            length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            return length;
+        }
+    }
+}
--- LineComp/Program.cs
+++ LineComp/Program.cs
@@ -2,34 +2,11 @@
 
 namespace LineComp
 {
-     public class Points
-        {
-            int x1, y1, x2, y2;
-        double length;
-
-           public Points(int x1, int y1, int x2, int y2)
-            {
-                this.x1 = x1;
-                this.y1 = y1;
-                this.x2 = x2;
-                this.y2 = y2;
-            }
-
-            public double calLength()
-            {
-                length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-                return length;
-            }
-
-        }
-
-
         class Program 
         { 
         static void Main(string[] args)
         { 
             Console.WriteLine("Welcome to Line Comparision Computation Using Class");
-            //Console.WriteLine("Enter the coordinates of pt1 and pt2");
             Points p1 = new Points(4,5,7,9);
             double l1 = p1.calLength();
             Console.WriteLine(l1);
@@ -38,7 +15,6 @@ namespace LineComp
             Console.WriteLine(l2);
             int k = l1.CompareTo(l2);
 
-            Console.WriteLine("check1");
             if (k.Equals(0))
             {
                 Console.WriteLine("Lines are equal");
@@ -46,7 +22,7 @@ namespace LineComp
 
             else
             {
-                if (k>1)
+                if (k==1)
                 {
                     Console.WriteLine("Line 1 is greater than Line 2");
                 }
